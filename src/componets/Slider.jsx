83859ac9eaeef9b492098aa294c89f0d04b1edb2
krishna soni <soni01krishna@gmail.com>
2023-08-07 14:29:04 +0530

    change in slider images

--- src/componets/Slider.jsx
+++ src/componets/Slider.jsx
@@ -6,7 +6,7 @@ export default function Slider() {
       <div id="carouselExample" class="carousel slide">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="https://www.snitch.co.in/cdn/shop/files/Orange_Modern_Promotional_Fashion_Spring_Sale_Banner_1_9280b17e-584f-464c-8768-3f5749118aff_1944x.png?v=1689345283" class="d-block w-100" alt="..."/>
+      <img src="https://www.snitch.co.in/cdn/shop/files/ezgif.com-gif-to-webp_1944x.webp?v=1691232496" class="d-block w-100" alt="..."/>
     </div>
     <div class="carousel-item">
       <img src="https://www.snitch.co.in/cdn/shop/files/new-arrival-banner-2_1944x.jpg?v=1689858333" class="d-block w-100" alt="..."/>
