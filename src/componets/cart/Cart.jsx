2d4e0e3e6c45f9b39b7316c14f85e3568a10fb0b
krishnaSONI011 <soni01krishna@gmail.com>
2023-08-15 13:13:48 +0530

    fixing cart

--- src/componets/cart/Cart.jsx
+++ src/componets/cart/Cart.jsx
@@ -1,52 +1,61 @@
-import React,{useState} from "react";
-
+import React, { useState, useEffect } from "react";
 import CartProduct from "./CartProduct";
 
-
 let Cart = (props) => {
-    let [total,setTotal] = useState(0)
-    function close(){
-        
+    let [total, setTotal] = useState(0);
 
-            props.setCart(-500)
-        
+    useEffect(() => {
+        // Calculate total price based on fetched cart items
+        let calculatedTotalPrice = 0;
+
+        props.item.forEach((cart) => {
+            calculatedTotalPrice += cart.qty * parseInt(cart.product.price);
+        });
+
+        setTotal(calculatedTotalPrice);
+    }, [props.item]);
+
+    function close() {
+        props.setCart(-500);
     }
-  
+
     return (
         <>
             <div className="sidebar overflow-hidden" style={{
-            right:0,
-            display:props.mode === -500 ? "none" : "block",
-            height:"100%"
+                right: 0,
+                display: props.mode === -500 ? "none" : "block",
+                height: "100%"
             }}>
                 <div className="inner-sidebar position-absolute" style={{
-                    right:props.mode+"%",
-                    width:"30%",
-                    height:"100%"
+                    right: props.mode + "%",
+                    width: "30%",
+                    height: "100%"
                 }}>
                     <div className="cart">
                         <div className="cart-title d-flex justify-content-between p-2 py-3 border-bottom">
                             <h3 className="font-1 fs-2">CART</h3>
                             <i className="bi bi-x fs-4 mouse-pointer" onClick={close}></i>
                         </div>
-                        <div className="for-product  border-bottom pt-3">
-                        {
-                                props.item.map((cart, index) => (
-                                    <CartProduct key={index} cart={cart} setTotal={setTotal}/>
-                                ))
-                            }
+                        <div className="for-product border-bottom pt-3">
+                            {props.item.map((cart, index) => (
+                                <CartProduct
+                                    key={index}
+                                    cart={cart}
+                                    setTotal={setTotal}
+                                    total={total}
+                                />
+                            ))}
                         </div>
                         <div className="subtotle d-flex justify-content-between py-3">
                             <h6 className="font-2 space py-2">SUBTOTAL</h6>
-                            <h6 className="font-1 ">Rs {total}</h6>
-                           
+                            <h6 className="font-1">Rs {total}</h6>
                         </div>
                         <p className="text-center" style={{ fontSize: "12px" }}>Shipping, taxes, and discount codes calculated at checkout. </p>
                         <div className="to-checkout">
-                            <button className="shine-button bg-dark font-2 space w-100 text-light">PLACED ORDER<br></br><span style={{fontSize:"16px"}}>UPI/COD</span></button>
+                            <button className="shine-button bg-dark font-2 space w-100 text-light">PLACED ORDER<br /><span style={{ fontSize: "16px" }}>UPI/COD</span></button>
                         </div>
                     </div>
-                 </div>
+                </div>
             </div>
         </>
     )
--- src/componets/cart/CartProduct.jsx
+++ src/componets/cart/CartProduct.jsx
@@ -1,14 +1,14 @@
-import React,{useState,useEffect} from "react";
+import React,{useEffect,useState} from "react";
 
 let CartProduct = (props) => {
-    let [quantity, setQuantity] = useState(props.cart.qty);
-    
+     const [quantity, setQuantity] = useState(parseInt(props.cart.qty));
+
     useEffect(() => {
-        // Calculate total price based on quantity and unit price
+        // Calculate and update the total for this specific cart item
         const unitPrice = parseInt(props.cart.product.price);
         const newTotal = unitPrice * quantity;
-        props.setTotal(newTotal);
-    }, [quantity, props.cart.product.price]);
+        props.setTotal(props.total + (newTotal - (unitPrice * props.cart.qty)));
+    }, [quantity]);
     return (
         <>
             <div className="cart-product d-flex justify-content-between my-3">
@@ -17,47 +17,19 @@ let CartProduct = (props) => {
                     <p>{props.cart.product.name}</p>
                     <span><b>Size:</b> {props.cart.size}</span>
                     <div className="d-flex align-items-center justify-content-between py-4 ">
-                    <div className="quantity ">
-            
-                            <div className="q-box border d-flex align-items-center justify-content-between " style={{
-                    width:"57%"
-                }}>
-                    <span
-                        className="fs-4    mouse-pointer q-point"
-                        onClick={() => {
-                            setQuantity(quantity + 1);
-                        }}
-                                    style={{
-                                        padding:"5px 10px"
-                                    }}
-                    >
-                        +
-                    </span>
-                    <span className="q-input text-center">
-                        <input
-                            type="text"
-                            value={quantity}
-                             
-                        />
-                    </span>
-                    <span
-                        className="fs-4   mouse-pointer q-point"
-                        style={{
-                            padding:"5px 10px"
-                        }}
-                        onClick={(e) => {
-                            setQuantity(quantity - 1)
-                        }}
-                    >
-                        -
-                    </span>
-                </div>
+                        <div className="quantity ">
+                            <div className="q-box border d-flex align-items-center justify-content-between" style={{ width: "57%" }}>
+                                <span className="fs-4 mouse-pointer q-point" onClick={() => { setQuantity(parseInt(quantity) + 1); }} style={{ padding: "5px 10px" }}>+</span>
+                                <span className="q-input text-center">
+                                    <input type="text" value={quantity} />
+                                </span>
+                                <span className="fs-4 mouse-pointer q-point" style={{ padding: "5px 10px" }} onClick={() => { if (parseInt(quantity)  > 1) { setQuantity(parseInt(quantity)  - 1); } }}>-</span>
+                            </div>
                         </div>
-                        <span style={{width:"34%"}}>Rs {props.cart.product.price}</span>
-                        </div>
+                        <span style={{ width: "34%" }}>Rs {props.cart.product.price}</span>
+                    </div>
                 </div>
-                
-                </div>
+            </div>
         </>
     )
 }
