faed421710e4e8cdba427e83c5a7c624b9f8133f
krishna soni <soni01krishna@gmail.com>
2023-09-11 13:08:47 +0530

    address model created

---
+++ backend/model/addressModel.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+const AddressSchema = new mongoose.Schema({
+    user:{
+        type:mongoose.ObjectId,
+        ref:"User",
+        required:true
+    },
+    firstname:{
+        type:String,
+        required:true
+    },
+    lastname:{
+        type:String,
+        required:true
+    },
+    addressLine1:{
+        type:String
+    },
+    addressLine2:{
+        type:String
+    },
+    company:{
+        type:String
+    },
+    postalCode:{
+        type:String
+    },
+    phone:{
+        type:String
+    },
+    city:{
+        type:String
+    },
+    country:{
+        type:String
+    },
+    state:{
+        type:String
+    }
+})
+export default mongoose.model("address",AddressSchema)
